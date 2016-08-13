8fdb4b289e3dc6c48ccc69ffbcadcdf180c81933
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-08-13 18:49:45 -0400

    terms listed and shown as links

--- schema.sql
+++ schema.sql
@@ -24,3 +24,15 @@ INSERT INTO terms VALUES (NULL, "Console", "Prints text to the console. Useful f
                                 "Codecademy", "https://www.codecademy.com/articles/glossary-javascript", 1);
 INSERT INTO terms VALUES (NULL, "Function", "A function is a JavaScript procedure—a set of statements that performs a task or calculates a value.It is like a reusable piece of code. Imagine , having 20 for loops ,and then having a single function to handle it all . To use a function, you must define it somewhere in the scope from which you wish to call it. A function definition (also called a function declaration) consists of the function keyword, followed by the name of the function, a list of arguments to the function, enclosed in parentheses and separated by commas, the JavaScript statements that define the function, enclosed in curly braces, { }.",
                                 "Codecademy", "https://www.codecademy.com/articles/glossary-javascript", 1);
+
+
+INSERT INTO terms VALUES (NULL, "find", "Using the find method, you can retrieve the object corresponding to the specified primary key that matches any supplied options.",
+                                "David Heinemeier Hansson", "http://guides.rubyonrails.org/active_record_querying.html", 2);
+INSERT INTO terms VALUES (NULL, "take", "The take method retrieves a record without any implicit ordering.",
+                                "David Heinemeier Hansson", "http://guides.rubyonrails.org/active_record_querying.html", 2);
+INSERT INTO terms VALUES (NULL, "first", "The first method finds the first record ordered by primary key (default).",
+                                "David Heinemeier Hansson", "http://guides.rubyonrails.org/active_record_querying.html", 2);
+INSERT INTO terms VALUES (NULL, "last", "The last method finds the last record ordered by primary key (default).",
+                                "David Heinemeier Hansson", "http://guides.rubyonrails.org/active_record_querying.html", 2);
+INSERT INTO terms VALUES (NULL, "find_by", "The find_by method finds the first record matching some conditions.",
+                                "David Heinemeier Hansson", "http://guides.rubyonrails.org/active_record_querying.html", 2);
--- testing.rb
+++ testing.rb
@@ -7,11 +7,22 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 
-terms = Term.all
-terms.each do
-  puts "#{terms.name}"
-end
+# terms = Term.all
+# terms.each do
+#   puts "#{terms.name}"
+# end
 
 get "/" do
+  @terms = Term.all
+  @categories = Category.all
+
+  @lastfive = @terms.last(5)
+
   haml :homepage
+end
+
+get "/terms" do
+  @terms = Term.all
+
+  haml :terms
 end
